9b41865aee595d4ee4fb6d154576a92e1cb87f00
Ilyas Boukhechem <ilyas.boukhechem@gmx.fr>
2025-02-24 22:52:08 +0100

    started to add profile recipe

--- src/components/recipe-profil/index.tsx
+++ src/components/recipe-profil/index.tsx
@@ -1,9 +1,37 @@
+import { selectRecipes } from "@/redux/selectors";
+import { Box } from "@mui/material";
+import Image from "next/image";
+import { useSelector } from "react-redux";
 import { StyledRecipeProfile } from "./styles";
 
 interface RecipeProfileProps {}
 
 const RecipeProfile = (props: RecipeProfileProps) => {
-  return <StyledRecipeProfile></StyledRecipeProfile>;
+  const { recipe } = useSelector(selectRecipes);
+
+  return (
+    recipe && (
+      <StyledRecipeProfile>
+        <Box className="profile-header">
+          <Box className="profile-image-container">
+            <Image
+              src={recipe.image}
+              alt={`This image came from that link ${recipe.name}`}
+              objectFit="cover"
+              objectPosition={"center"}
+              layout="fill"
+              priority
+            />
+          </Box>
+        </Box>
+        <ul>
+          {recipe.instructions.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </StyledRecipeProfile>
+    )
+  );
 };
 
 export default RecipeProfile;
--- src/components/recipe-profil/styles.tsx
+++ src/components/recipe-profil/styles.tsx
@@ -1,3 +1,18 @@
 import { Box, styled } from "@mui/material";
 
-export const StyledRecipeProfile = styled(Box)``;
+export const StyledRecipeProfile = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  gap: 48px;
+  .profile-header {
+    display: flex;
+    justify-content: center;
+    .profile-image-container {
+      position: relative;
+      width: 400px;
+      height: 400px;
+      border-radius: 12px;
+      overflow: hidden;
+    }
+  }
+`;
